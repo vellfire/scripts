6d5826ccd2f059ff29671979c540fffac7192f32
Veelhie <william.mcdade@wmhm.me>
2023-01-30 23:33:21 +0000

    Change date/time to variable

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@
 BACKUPDIR=/mnt/backups
 IMAGEDIR=/mnt/mx500_raid1/libvirt/images
 PHOSTNAME=$(hostnamectl status | grep -w "Pretty hostname:" | awk '{print $3}')
+CDATE=$(date +"%Y%m%dT%H%M")
 
 state () {
     JOBSTATE=$(virsh domjobinfo "${VM}" | grep -w "Job type:" | awk '{print $3}')
@@ -21,7 +22,7 @@ for VM in $(virsh list --all --name); do
 JOBSTATE=$(virsh domjobinfo "${VM}" | grep -w "Job type:" | awk '{print $3}') 
 if [ "$JOBSTATE" == "None" ];  then
     unset UT
-    virsh dumpxml "${VM}" > "$BACKUPDIR/$PHOSTNAME/${VM}/${VM}_$(date +"%Y%m%dT%H%M").xml";
+    virsh dumpxml "${VM}" > "$BACKUPDIR/$PHOSTNAME/${VM}/${VM}_${CDATE}.xml";
     export UT=$(date +%s); virsh backup-begin "${VM}"
     sleep 3
     state
@@ -29,8 +30,8 @@ if [ "$JOBSTATE" == "None" ];  then
         sleep 5
         state
     done
-    mv $IMAGEDIR/"${VM}.qcow2.${UT}" "$BACKUPDIR/$PHOSTNAME/${VM}/${VM}_$(date +"%Y%m%dT%H%M").qcow2"
-elif [ ! -f "$IMAGEDIR/$VM.qcow2" ]; then
-    virsh dumpxml "${VM}" > "$BACKUPDIR/$PHOSTNAME/$VM/${VM}_$(date +"%Y%m%dT%H%M").xml"
+    mv $IMAGEDIR/"${VM}.qcow2.${UT}" "$BACKUPDIR/$PHOSTNAME/${VM}/${VM}_${CDATE}.qcow2"
+elif [ ! -f "$IMAGEDIR/${VM}.qcow2" ]; then
+    virsh dumpxml "${VM}" > "$BACKUPDIR/$PHOSTNAME/${VM}/${VM}_${CDATE}.xml"
 fi
 done
