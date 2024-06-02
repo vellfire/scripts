9295aa793d50d12d12e7d6ea91f3de9fbbf3e0a2
Vellfire_ <will@wmhm.me>
2024-06-02 19:31:57 +0100

    remove unnecessary quotes

--- virtbackup.sh
+++ virtbackup.sh
@@ -17,6 +17,6 @@ VM_BACKUP_LIST=$(echo "$VM_LIST" | grep -vE "$(IFS="|"; echo "${EXCLUDE_LIST[*]}
 
 # Make directories and run backups
 for VM in $VM_BACKUP_LIST; do
-    mkdir -p "$BACKUP_DIR/$HOSTNAME/$VM/$CURRENT_MONTH"
-    virtnbdbackup -U $QEMU_URI -d "$VM" -l auto -o "$BACKUP_DIR/$HOSTNAME/$VM/$CURRENT_MONTH" -z
+    mkdir -p $BACKUP_DIR/$HOSTNAME/$VM/$CURRENT_MONTH
+    virtnbdbackup -U $QEMU_URI -d $VM -l auto -o $BACKUP_DIR/$HOSTNAME/$VM/$CURRENT_MONTH -z
 done
